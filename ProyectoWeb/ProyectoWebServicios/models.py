26b89f5f9ff5b7abb9f5a424b1b6bd2decefcf3c
arobynd <arobynd@gmail.com>
2022-09-13 21:12:11 -0500

    service migrations

--- ProyectoWeb/ProyectoWebServicios/models.py
+++ ProyectoWeb/ProyectoWebServicios/models.py
@@ -1,3 +1,14 @@
+from email.headerregistry import ContentDispositionHeader
 from django.db import models
 
 # Create your models here.
+class Servicios(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    titulo = models.CharField(max_length=80)
+    contenido = models.CharField(max_length=80)
+    imagen = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo

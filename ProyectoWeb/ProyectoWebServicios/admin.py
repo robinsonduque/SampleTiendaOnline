dd21cc409800cd65e1662a3d1a272bb33b8b7fe7
arobynd <arobynd@gmail.com>
2022-09-14 19:12:30 -0500

    bug fix

--- ProyectoWeb/ProyectoWebServicios/admin.py
+++ ProyectoWeb/ProyectoWebServicios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from ProyectoWebServicios.models import Servicios
+
 # Register your models here.
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ("titulo", "contenido")
+    readonly_fields = ("created", "updated")
+
+
+admin.site.register(Servicios, AuthorAdmin)
--- ProyectoWeb/ProyectoWebServicios/models.py
+++ ProyectoWeb/ProyectoWebServicios/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from email.headerregistry import ContentDispositionHeader
 from django.db import models
 
@@ -6,9 +7,11 @@ class Servicios(models.Model):
     id = models.BigAutoField(primary_key=True)
     titulo = models.CharField(max_length=80)
     contenido = models.CharField(max_length=80)
-    imagen = models.ImageField()
+    imagen = models.ImageField(
+        upload_to="servicios"
+    )  # se configuró en settings para que cargue las imagenes en este directorio
     created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.titulo

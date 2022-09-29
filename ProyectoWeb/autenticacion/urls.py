a3e92c9c02bf2dc454f9c6d7379d110848da0637
arobynd <arobynd@gmail.com>
2022-09-29 08:17:07 -0500

    session start

--- ProyectoWeb/autenticacion/urls.py
+++ ProyectoWeb/autenticacion/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import Registro
+from .views import Registro, cerrar_sesion, iniciar_sesion
 
 
 app_name = "autenticacion"
 
 urlpatterns = [
     path("autenticar/", Registro.as_view(), name="registrar"),
+    path("cerrar_sesion/", cerrar_sesion, name="cerrar_sesion"),
+    path("iniciar_sesion/", iniciar_sesion, name="iniciar_sesion"),
 ]
--- ProyectoWeb/autenticacion/views.py
+++ ProyectoWeb/autenticacion/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import View
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
 from django.contrib import messages
 
 
@@ -19,3 +19,30 @@ class Registro(View):
             return redirect("tienda")
         else:
             return render(request, "registro/registro.html", {"form": form})
+
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect("/")
+
+
+def iniciar_sesion(request):
+
+    if request.method == "GET":
+        form = AuthenticationForm()
+        return render(request, "login/iniciar_sesion.html", {"form": form})
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            nombre = form.cleaned_data.get("username")
+            contra = form.cleaned_data.get("password")
+            usuario = authenticate(username=nombre, password=contra)
+            if usuario is not None:
+                login(request, usuario)
+                return redirect("inicio")
+            else:
+                messages.error(request, "Usuario no válido")
+        else:
+            messages.error(request, "Información incorrecta")
+
+        return render(request, "login/iniciar_sesion.html", {"form": form})
